faa775b2375d15e16240670ecf585b3fc7c5df7b
thuyoanhhh1312 <thuyoanhhh1312@gmail.com>
2026-04-05 13:04:16 +0700

    CMJW-56 [Frontend] UI danh sách & chi tiết đơn hàng

--- client/src/api/orderApi.js
+++ client/src/api/orderApi.js
@@ -2,6 +2,19 @@ import axiosInstance from "./axiosInstance";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
+const getOrderByCustomer = async (userId, accessToken) => {
+  const response = await axiosInstance.get(
+    `${API_URL}/orders/by-customer/${userId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
+
+  return response.data;
+};
+
 const calculatePrice = async (payload, accessToken) => {
   const response = await axiosInstance.post(`${API_URL}/calculate-price`, payload, {
     headers: {
@@ -13,5 +26,6 @@ const calculatePrice = async (payload, accessToken) => {
 };
 
 export default {
+  getOrderByCustomer,
   calculatePrice,
 };
